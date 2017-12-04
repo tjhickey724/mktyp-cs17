a437f6636281369734af0b372eee56fc9c12343f
tim <tjhickey@brandeis.edu>
2017-12-04 13:17:17 -0500

    added authentication

--- mktypapp.py
+++ mktypapp.py
@@ -1,12 +1,111 @@
-from flask import Flask, render_template, request
-from datetime import datetime
+"""
+    google example
+    ~~~~~~~~~~~~~~
+
+    This example is contributed by Bruno Rocha
+
+    GitHub: https://github.com/rochacbruno
+"""
+from functools import wraps
+from flask import Flask, redirect, url_for, session, request, jsonify, render_template, request
+from flask_oauthlib.client import OAuth
+
 
 app = Flask(__name__)
+app.config['GOOGLE_ID'] = '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com'
+app.config['GOOGLE_SECRET'] = 'iqgLqu6pXgLuHsZFq6nvxDX3'
+
+
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+google = oauth.remote_app(
+    'google',
+    consumer_key=app.config.get('GOOGLE_ID'),
+    consumer_secret=app.config.get('GOOGLE_SECRET'),
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
+
+def require_login(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not('google_token' in session):
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/main')
+def index():
+    if 'google_token' in session:
+        me = google.get('userinfo')
+        print("logged in")
+        print(jsonify(me.data))
+        return render_template("main.html")
+        #return jsonify({"data": me.data})
+    print('redirecting')
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return google.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('google_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = google.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['google_token'] = (resp['access_token'], '')
+    print(session['google_token'])
+    me = google.get('userinfo')
+    session['userinfo'] = me.data
+    print(me.data)
+    return render_template("main.html")
+    #return jsonify({"data": me.data})
+
+
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
+
+
+#if __name__ == '__main__':
+#    app.run()
+
+
+#from flask import Flask, render_template, request
+
+
+
+#app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-	return render_template("main.html")
+	logged_in = ('google_token' in session)
+	if logged_in:
+		print("logged in")
+	else:
+		print("not logged in")
+	return render_template("main.html",status=logged_in)
 
 @app.route('/team')
 def bio():
@@ -20,14 +119,20 @@ def pitch():
 def formdemo():
 	return render_template('formdemo.html')
 
+from datetime import datetime
 messages=[]
+
 @app.route('/chat',methods=['GET','POST'])
+@require_login
 def chat():
 	if request.method == 'POST':
 		msg = request.form['msg']
-		who = request.form['who']
+		userinfo = session['userinfo']
+		print(userinfo)
+		who = userinfo['email']
 		now = datetime.now()
 		x = {'msg':msg,'now':now,'who':who}
+		print(x)
 		messages.insert(0,x) # add msg to the front of the list
 		return render_template("chat.html",messages=messages)
 	else:
@@ -37,4 +142,4 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=3000)
+    app.run('0.0.0.0',port=5000)
